4cbef5a96f990e5a7627d663154997bafcd190f7
mehmetistif <mehmetistif@gmail.com>
2020-01-28 14:54:27 +0300

    installer eklendi, teslimat detay servisinde hsdat exception düzeltildi

---
+++ Sultanlar/Sultanlar.WindowsServiceIslemler/ProjectInstaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+
+namespace Sultanlar.WindowsServiceIslemler
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            this.serviceInstaller1.ServiceName = "Sultanlar Windows Servis";
+            this.serviceInstaller1.DisplayName = "Sultanlar Windows Servis";
+            this.serviceProcessInstaller1.Account = ServiceAccount.LocalSystem;
+        }
+    }
+}
