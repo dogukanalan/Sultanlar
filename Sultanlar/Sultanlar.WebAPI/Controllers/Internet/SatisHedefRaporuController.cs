657ae22d7d10ae141c9bbc1febb1d4f05fbd1eee
Mehmet <Mehmet@DESKTOP-FP1KRES>
2022-03-09 15:57:16 +0300

    webapi ve wui dashboard satis-hedef

--- Sultanlar/Sultanlar.WebAPI/Controllers/Internet/SatisHedefRaporuController.cs
+++ Sultanlar/Sultanlar.WebAPI/Controllers/Internet/SatisHedefRaporuController.cs
@@ -15,5 +15,31 @@ namespace Sultanlar.WebAPI.Controllers.Internet
     {
         [HttpGet, Route("internet/[controller]/[action]/{YIL}/{AY}/{SLSREF}")]
         public List<satisHedefRaporu> Getir(int YIL, int AY, int SLSREF) => new SatisHedefRaporuProvider().SatisHedefRapor(YIL, AY, SLSREF);
+
+        [HttpGet, Route("internet/[controller]/[action]/{YIL}/{SLSREF}")]
+        public IEnumerable<int> SatisForDashboard(int YIL, int SLSREF)
+        {
+            var satis = new SatisHedefRaporuProvider().DashboardYillikSatis(YIL, SLSREF);
+            var results = new int[12];
+            for (int i = 0; i < 12; i++)
+            {
+                if (i < satis.Count)
+                    results[i] = Convert.ToInt32(satis[i].SATIS);
+            }
+            return results;
+        }
+
+        [HttpGet, Route("internet/[controller]/[action]/{YIL}/{SLSREF}")]
+        public IEnumerable<int> HedefForDashboard(int YIL, int SLSREF)
+        {
+            var hedef = new SatisHedefRaporuProvider().DashboardYillikHedef(YIL, SLSREF);
+            var results = new int[12];
+            for (int i = 0; i < 12; i++)
+            {
+                if (i < hedef.Count)
+                    results[i] = Convert.ToInt32(hedef[i].HEDEF);
+            }
+            return results;
+        }
     }
 }
