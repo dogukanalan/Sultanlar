3b5b5b7b057b8a85fefca59efc94946ca3311ed2
Mehmet <Mehmet@DESKTOP-FP1KRES>
2022-04-04 14:18:25 +0300

    wui hizmet bedelleri tutar excel, ui tp anlasma yetki duzeltme

--- Sultanlar/Sultanlar.UI/frmINTERNETticaripazarlamaanlasmalar.cs
+++ Sultanlar/Sultanlar.UI/frmINTERNETticaripazarlamaanlasmalar.cs
@@ -246,7 +246,7 @@ namespace Sultanlar.UI
                 int AnlasmaID = Convert.ToInt32(((DataRowView)gridControl1.MainView.GetRow(gridView1.GetSelectedRows()[0])).Row.ItemArray[0]);
                 Anlasmalar anlasma = Anlasmalar.GetObject(AnlasmaID);
 
-                if (frmAna.KAdi == "ST47" || frmAna.KAdi == "nermincelik" || frmAna.KAdi == "ST01")
+                if (frmAna.KAdi == "ST47" || frmAna.KAdi == "NERMİNCELİK" || frmAna.KAdi == "ST01")
                 {
                     if (MessageBox.Show("Anlaşma onaylanacak, devam etmek istediğinize emin misiniz?", "Onaylama", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == System.Windows.Forms.DialogResult.Yes)
                     {
