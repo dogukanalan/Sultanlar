f5bf05d8b6a190a9267bdb96ee1da6da69d9c373
mistif.sultanlar <mistif.sultanlar@gmail.com>
2022-05-31 14:25:48 +0300

    wui iade incele duzeltme

--- Sultanlar/Sultanlar.WUI/wwwroot/js/main.js
+++ Sultanlar/Sultanlar.WUI/wwwroot/js/main.js
@@ -243,22 +243,22 @@ var inceleonaylasilcolumns = [
     },
     {
         "data": null, "class": window.location.href.indexOf("fiyattipi=2") > -1 ? "floaTd hidewhenmobile" : "hide", render: function (data, type, row) {
-            return '<span class="sinirli isk1">' + data.isks.isK1.formatMoney(3, ',', '.') + '</span>';
+            return '<span class="sinirli isk1">' + (data.isks ? data.isks.isK1.formatMoney(3, ',', '.') : '') + '</span>';
         }
     },
     {
         "data": null, "class": window.location.href.indexOf("fiyattipi=2") > -1 ? "floaTd hidewhenmobile" : "hide", render: function (data, type, row) {
-            return '<span class="sinirli isk2">' + data.isks.isK2.formatMoney(3, ',', '.') + '</span>';
+            return '<span class="sinirli isk2">' + (data.isks ? data.isks.isK2.formatMoney(3, ',', '.') : '') + '</span>';
         }
     },
     {
         "data": null, "class": window.location.href.indexOf("fiyattipi=2") > -1 ? "floaTd hidewhenmobile" : "hide", render: function (data, type, row) {
-            return '<span class="sinirli isk3">' + data.isks.isK3.formatMoney(3, ',', '.') + '</span>';
+            return '<span class="sinirli isk3">' + (data.isks ? data.isks.isK3.formatMoney(3, ',', '.') : '') + '</span>';
         }
     },
     {
         "data": null, "class": window.location.href.indexOf("fiyattipi=2") > -1 ? "floaTd hidewhenmobile" : "hide", render: function (data, type, row) {
-            return '<span class="sinirli isk4">' + data.isks.isK4.formatMoney(3, ',', '.') + '</span>';
+            return '<span class="sinirli isk4">' + (data.isks ? data.isks.isK4.formatMoney(3, ',', '.') : '') + '</span>';
         }
     },
     {
@@ -267,13 +267,13 @@ var inceleonaylasilcolumns = [
         }
     },
     {
-        "data": null, "class": "floaTd hidewhenmobile", render: function (data, type, row) {
-            return '<span class="sinirli">' + iskDusCoklu((data.mnFiyat), data.isks.isK1, data.isks.isK2, data.isks.isK3, data.isks.isK4).formatMoney(2, ',', '.') + '</span>';
+        "data": null, "class": window.location.href.indexOf("fiyattipi") > -1 ? "floaTd hidewhenmobile" : "hide", render: function (data, type, row) {
+            return '<span class="sinirli">' + (data.isks ? iskDusCoklu((data.mnFiyat), data.isks.isK1, data.isks.isK2, data.isks.isK3, data.isks.isK4).formatMoney(2, ',', '.') : '') + '</span>';
         }
     },
     {
-        "data": null, "class": "floaTd hidewhenmobile", render: function (data, type, row) {
-            return '<span class="sinirli">' + iskDusCoklu((data.mnFiyat * data.intMiktar), data.isks.isK1, data.isks.isK2, data.isks.isK3, data.isks.isK4).formatMoney(2, ',', '.') + '</span>';
+        "data": null, "class": window.location.href.indexOf("fiyattipi") > -1 ? "floaTd hidewhenmobile" : "hide", render: function (data, type, row) {
+            return '<span class="sinirli">' + (data.isks ? iskDusCoklu((data.mnFiyat * data.intMiktar), data.isks.isK1, data.isks.isK2, data.isks.isK3, data.isks.isK4).formatMoney(2, ',', '.') : '') + '</span>';
         }
     }
 ];
