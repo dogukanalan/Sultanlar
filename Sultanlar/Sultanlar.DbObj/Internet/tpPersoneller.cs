8ac0fd45b433d74bb7c5d967cb48281e10ca568e
Doğukan Alan <dogukanalan25@gmail.com>
2023-04-18 16:14:48 +0300

    tp personel class olusturma baslangici guncellemesi

---
+++ Sultanlar/Sultanlar.DbObj/Internet/tpPersoneller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sultanlar.DbObj.Internet
+{
+    public class tpPersonelBaglantilari : DbObj
+    {
+
+    }
+
+    public class tpPersoneller : DbObj
+    {
+
+    }
+
+    public class tpPersonelTurleri : DbObj
+    {
+        public int pkID { get; set; }
+        public string strTur { get; set; } 
+        public string strAciklama { get; set; }
+        public tpPersonelTurleri() { }
+        public tpPersonelTurleri(byte pkID) { this.pkID = pkID; }
+        public tpPersonelTurleri(string strTur, string strAciklama)
+        {
+            this.strTur = strTur;
+            this.strAciklama = strAciklama;
+        }
+    }
+}
